9876892a81f4b08898ea68283d455265aae1f2ea
Razadan1 <danmoleomomayowa@gmail.com>
2024-05-21 12:39:30 +0100

    Base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,14 +1,19 @@
+#!/usr/bin/env python3
+""" This module is the Base model class"""
 from uuid import uuid4
 from datetime import datetime
 
+
+
 class BaseModel:
+    """ This is the Base Model class which other classes will inherit from"""
     def __init__(self) -> None:
         self.id=str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self) -> str:
-        return f"[{self.__class__.__name__}] (self.id) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
     
     def save(self) -> None:
         self.updated_at = datetime.now()
